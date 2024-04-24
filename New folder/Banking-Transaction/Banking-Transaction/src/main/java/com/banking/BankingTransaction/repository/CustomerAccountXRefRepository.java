7aa967bde6d4efcb19110818b8bc1040d2210c6c
kiran <kiranbosco@gmail.com>
2024-04-24 21:07:33 +0530

    extra files are removed

--- New folder/Banking-Transaction/Banking-Transaction/src/main/java/com/banking/BankingTransaction/repository/CustomerAccountXRefRepository.java
+++
@@ -1,9 +0,0 @@
-package com.banking.BankingTransaction.repository;
-import com.banking.BankingTransaction.model.CustomerAccountXRef;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface CustomerAccountXRefRepository extends CrudRepository<CustomerAccountXRef, String> {
-}
