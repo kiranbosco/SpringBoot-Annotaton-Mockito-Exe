b3a08ddae9ed1e860d469a706ee0a87e306cf5b9
kiran <kiranbosco@gmail.com>
2024-04-26 19:05:28 +0530

    Put service created

--- Banking-Transaction/Banking-Transaction/src/main/java/com/purinis/BankingTransaction/service/BankingServiceImpl.java
+++ Banking-Transaction/Banking-Transaction/src/main/java/com/purinis/BankingTransaction/service/BankingServiceImpl.java
@@ -5,6 +5,8 @@ import com.purinis.BankingTransaction.domain.CustomerDetails;
 import com.purinis.BankingTransaction.domain.TransactionDetails;
 import com.purinis.BankingTransaction.domain.TransferDetails;
 import com.purinis.BankingTransaction.exception.BankingRuntimeException;
+import com.purinis.BankingTransaction.model.Address;
+import com.purinis.BankingTransaction.model.Contact;
 import com.purinis.BankingTransaction.model.Customer;
 import com.purinis.BankingTransaction.repository.AccountRepository;
 import com.purinis.BankingTransaction.repository.CustomerAccountXRefRepository;
@@ -76,12 +78,58 @@ public class BankingServiceImpl implements CustomerService {
 
     @Override
     public CustomerDetails findByCustomerNumber(Long customerNumber) {
-        return null;
+        Optional<Customer> optionalCustomer =  this.customerRepository.findByCustomerNumber(customerNumber);
+        if(!optionalCustomer.isPresent()){
+            throw new BankingRuntimeException(ApplicationConstant.USER_NOT_FOUND);
+        }
+        else {
+            return this.bankingServiceHelper.convertToCustomerDomain(optionalCustomer.get());
+        }
     }
 
     @Override
     public ResponseEntity<Object> updateCustomer(CustomerDetails customerDetails, Long customerNumber) {
-        return null;
+        Optional<Customer> manageCustomerEntityOption = this.customerRepository.findByCustomerNumber(customerNumber);
+        Customer unmanageCustomerEntity = this.bankingServiceHelper.convertToCustomerEntity(customerDetails);
+        if(!manageCustomerEntityOption.isPresent()){
+            throw new BankingRuntimeException(ApplicationConstant.USER_NOT_FOUND);
+        }
+        else {
+            // if customer available update the service
+            Customer manageCustomerEntity= manageCustomerEntityOption.get();
+            //update
+            if(Optional.ofNullable(manageCustomerEntity.getContactDetails()).isPresent()){
+                Contact mangaeContact = unmanageCustomerEntity.getContactDetails();
+                if(mangaeContact!=null){mangaeContact.setEmailId(mangaeContact.getEmailId());
+                    mangaeContact.setEmailId(manageCustomerEntity.getContactDetails().getEmailId());
+                    mangaeContact.setHomePhone(manageCustomerEntity.getContactDetails().getHomePhone());
+                    mangaeContact.setHomePhone(manageCustomerEntity.getContactDetails().getHomePhone());
+                }
+                else {
+                    unmanageCustomerEntity.setContactDetails(manageCustomerEntity.getContactDetails());
+                }
+                if (Optional.ofNullable(manageCustomerEntity.getCustomerAddress()).isPresent()) {
+                    Address manageAddress = unmanageCustomerEntity.getCustomerAddress();
+                    if(manageAddress!=null){
+                        manageAddress.setAddress1(manageCustomerEntity.getCustomerAddress().getAddress1());
+                        manageAddress.setAddress2(manageCustomerEntity.getCustomerAddress().getAddress2());
+                        manageAddress.setState(manageCustomerEntity.getCustomerAddress().getState());
+                        manageAddress.setCity(manageCustomerEntity.getCustomerAddress().getCity());
+                        manageAddress.setZip(manageCustomerEntity.getCustomerAddress().getZip());
+                        manageAddress.setCountry(manageCustomerEntity.getCustomerAddress().getCountry());
+                    }
+                    else {
+                        unmanageCustomerEntity.setCustomerAddress(manageCustomerEntity.getCustomerAddress());
+                    }
+                    manageCustomerEntity.setUpdateDateTime(new Date());
+                    manageCustomerEntity.setFirstName(unmanageCustomerEntity.getFirstName());
+                    manageCustomerEntity.setMiddleName(unmanageCustomerEntity.getMiddleName());
+                    manageCustomerEntity.setLastName(unmanageCustomerEntity.getLastName());
+                    manageCustomerEntity.setStatus(unmanageCustomerEntity.getStatus());
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApplicationConstant.CUSTMER_UPT);
     }
 
     @Override
