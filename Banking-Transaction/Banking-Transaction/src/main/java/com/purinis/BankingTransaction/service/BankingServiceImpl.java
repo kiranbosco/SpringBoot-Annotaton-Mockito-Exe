01eb471d62e2ecd9c8b31bad75aeb3cd072931f7
kiran <kiranbosco@gmail.com>
2024-04-26 17:26:39 +0530

    Services improved

--- Banking-Transaction/Banking-Transaction/src/main/java/com/purinis/BankingTransaction/service/BankingServiceImpl.java
+++ Banking-Transaction/Banking-Transaction/src/main/java/com/purinis/BankingTransaction/service/BankingServiceImpl.java
@@ -1,46 +1,77 @@
 package com.purinis.BankingTransaction.service;
+
 import com.purinis.BankingTransaction.domain.AccountInformation;
 import com.purinis.BankingTransaction.domain.CustomerDetails;
 import com.purinis.BankingTransaction.domain.TransactionDetails;
 import com.purinis.BankingTransaction.domain.TransferDetails;
+import com.purinis.BankingTransaction.exception.BankingRuntimeException;
+import com.purinis.BankingTransaction.model.Customer;
 import com.purinis.BankingTransaction.repository.AccountRepository;
 import com.purinis.BankingTransaction.repository.CustomerAccountXRefRepository;
 import com.purinis.BankingTransaction.repository.CustomerRepository;
 import com.purinis.BankingTransaction.repository.TransactionRepository;
-import jakarta.transaction.Transactional;
+import com.purinis.BankingTransaction.utils.ApplicationConstant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
 @Service
-@Transactional
+
 public class BankingServiceImpl implements CustomerService {
 
-    @Autowired
+
     private CustomerRepository customerRepository;
-    @Autowired
+
     private AccountRepository accountRepository;
-    @Autowired
+
     private TransactionRepository transactionRepository;
-    @Autowired
+
     private CustomerAccountXRefRepository custAccXRefRepository;
-    @Autowired
+
     private BankingServiceHelper bankingServiceHelper;
 
-    public BankingServiceImpl(CustomerRepository repository) {
+    @Autowired
+    public BankingServiceImpl(CustomerRepository repository,
+                              AccountRepository accountRepository,
+                              TransactionRepository transactionRepository,
+                              CustomerAccountXRefRepository customerAccountXRefRepository,
+                              BankingServiceHelper bankingServiceHelper) {
         this.customerRepository=repository;
+        this.bankingServiceHelper=bankingServiceHelper;
+        this.custAccXRefRepository=custAccXRefRepository;
+        this.transactionRepository=transactionRepository;
+        accountRepository=accountRepository;
+
     }
-
-
     @Override
     public List<CustomerDetails> findAll() {
-        return List.of();
+        List<CustomerDetails> customerDetails = new ArrayList<>();
+        Iterable<Customer> customersList =this.customerRepository.findAll();
+
+        customersList.forEach((customer -> {
+            customerDetails.add(this.bankingServiceHelper.convertToCustomerDomain(customer));
+        }));
+        return customerDetails;
     }
 
     @Override
     public ResponseEntity<Object> addCustomer(CustomerDetails customerDetails) {
-        return null;
+        Customer customer =  this.bankingServiceHelper.convertToCustomerEntity(customerDetails);
+        Optional<Customer> optionalCustomer = this.customerRepository.findByCustomerNumber(customer.getCustomerNumber());
+        if(!optionalCustomer.isPresent()){
+            customer.setCreateDateTime(new Date());
+            this.customerRepository.save(customer);
+        }
+        else {
+            throw new BankingRuntimeException(ApplicationConstant.USER_EXISTED);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApplicationConstant.USER_CREATED);
     }
 
     @Override
