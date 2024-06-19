8276ce245b8702b397d43c18c48d07f04d6c2d3d
kiran <kiranbosco@gmail.com>
2024-06-19 14:49:16 +0530

    Junit mockito all the code deploy

--- Spring boot annotations/Spring-boot-annotations/src/test/java/com/annotations/Spring/boot/annotations/SpringBootAnnotationsApplicationTests.java
+++ Spring boot annotations/Spring-boot-annotations/src/test/java/com/annotations/Spring/boot/annotations/SpringBootAnnotationsApplicationTests.java
@@ -1,13 +1,41 @@
 package com.annotations.Spring.boot.annotations;
 
+import com.annotations.Spring.boot.annotations.repo.UserRepo;
+import com.annotations.Spring.boot.annotations.service.Userservice;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringBootAnnotationsApplicationTests.class)
+@TestPropertySource("/insertData.sql")
+
 class SpringBootAnnotationsApplicationTests {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private Userservice userservice;
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@BeforeEach
+	public void testExecuteBefore(){
+
+		jdbcTemplate.execute("insert into users(id, name,product)" + "values(1,'kiran','Asus')");
+
+	}
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test(){
+
 	}
 
 }
